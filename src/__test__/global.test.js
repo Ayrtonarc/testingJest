eb868575cef1d3a84f7b7b474219aafac4d58bac
Ayrtonarc <ayrton.arc@gmail.com>
2022-03-21 22:01:26 -0700

    Jest prueba con callback

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -8,3 +8,22 @@ test('debe contener un texto',() => {
 test('debe contener Banana', () =>{
     expect(fruits).toContain('banana');
 });
+
+test('Mayor que', () => {
+ expect(10).toBeGreaterThan(9);
+});
+
+test('Verdadero', () => {
+    expect(true).toBeTruthy(); 
+    //no se le pasa valor por que va a validar que lo que se le pasa es un boleano
+});
+
+const reverseString = (str, callback) => { //callback
+    callback(str.split("").reverse().join(""))
+};
+
+test('Probar un callback', ()=> {
+    reverseString('Hola', (str)=> {
+        expect(str).toBe('aloH');
+    });
+});
