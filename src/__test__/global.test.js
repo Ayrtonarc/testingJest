e3285769219b0519fe51f5fe83998abf837c5729
Ayrtonarc <ayrton.arc@gmail.com>
2022-03-21 21:18:03 -0700

    Jest prueba array

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,10 @@
 const  text  = "Hola Mundo";
+const fruits = ['manzana','melon','banana'];
 
 test('debe contener un texto',() => {
     expect(text).toMatch(/Mundo/);
+});
+
+test('debe contener Banana', () =>{
+    expect(fruits).toContain('banana');
 });
